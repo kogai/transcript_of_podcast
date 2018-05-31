a9d5d678d508a205cc989c8b5339b265fabe926e
Shinichi Kogai <kogai0121@gmail.com>
2018-05-31 16:19:08 +0900

    tmp

--- run.sh
+++ run.sh
@@ -1,14 +1,10 @@
 #! /bin/bash
-eval "$(ssh-agent -s)"
-ssh-add /app/secrets/.ssh/id_rsa
 
-node src/Main.bs.js
+STATUS=$(git status -s)
+echo $STATUS
 
-git config --global user.email "kogai0121@gmail.com"
-git config --global user.name "Auto updater"
-git checkout -b update/$(date -I)
-git add audio/
-git commit -m ":robot: Add new episode"
-git push
-
-node src/GitHub.bs.js
+if [ "$STATUS" != "" ]; then
+  echo "DIFF EXIST"
+else
+  echo "DIFF NOT EXIST"
+fi
