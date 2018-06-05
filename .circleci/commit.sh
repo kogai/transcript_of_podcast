da08df499f47d1ceca1856c7e0dadd4d3d38549c
Shinichi Kogai <kogai0121@gmail.com>
2018-06-05 14:05:45 +0900

    Add new ep of ReasonTown

--- .circleci/commit.sh
+++ .circleci/commit.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-<<<<<<< HEAD
-if [ "$STATUS" != "" ]; then
-  echo "DIFF EXIST"\
-  git add audio
-  git commit -m ":robot: Add new episode"
-  git push --set-upstream origin use-ci-as-batch
-=======
 STATUS=$(git status -s)
 BRANCH="update/$(date -I)"
 if [ "$STATUS" != "" ]; then
@@ -15,7 +8,6 @@ if [ "$STATUS" != "" ]; then
   git add audio/*
   git commit -m ":robot: Add new episode"
   git push --set-upstream origin $BRANCH
->>>>>>> 530703221e647f861b67ddca9a4c3bd6c73ee480
 else
   echo "DIFF NOT EXIST"
 fi
