0bef38d454bcb7a7ef04e8c8ae01609d16095466
Shinichi Kogai <kogai0121@gmail.com>
2018-06-16 14:23:28 +0900

    Commit to master directry

--- .circleci/commit.sh
+++ .circleci/commit.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 STATUS=$(git status -s)
-BRANCH="update/$(date -I)"
+BRANCH=master
 if [ "$STATUS" != "" ]; then
   echo "DIFF EXIST"
-  git checkout -b $BRANCH
   git add audio/*
   git commit -m ":robot: Add new episode"
   git push --set-upstream origin $BRANCH
