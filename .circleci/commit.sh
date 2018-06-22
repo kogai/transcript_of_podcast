4a69d7127fecabad822ac956056013b2906f28ff
Shinichi Kogai <kogai0121@gmail.com>
2018-06-22 13:08:31 +0900

    Send notification to Slack

--- .circleci/commit.sh
+++ .circleci/commit.sh
@@ -7,6 +7,9 @@ if [ "$STATUS" != "" ]; then
   git add audio/*
   git commit -m ":robot: Add new episode"
   git push --set-upstream origin $BRANCH
+  curl -X POST \
+    --data-urlencode "payload={\"channel\": \"#general\", \"username\": \"Bot\", \"text\": \"Transcript has been updated\", \"icon_emoji\": \":ghost:\"}" \
+    "$SLACK_WEBHOOK"
 else
   echo "DIFF NOT EXIST"
 fi
